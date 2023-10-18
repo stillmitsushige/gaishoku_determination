36629013b09a0885e50653c90007b9b4ed4571e3
mitsushige <mtsg.dfm@gmail.com>
2023-10-18 19:09:15 +0900

    リッチに

--- js/gaishoku.js
+++ js/gaishoku.js
@@ -1,21 +1,23 @@
 function displayMessage() {
     var age = document.getElementById("age").value;
     var meal = document.getElementById("meal").value;
-    var message = "";
+    var messageElement = document.getElementById("message");
+
+    messageElement.innerHTML = "";
+    messageElement.className = "";
 
     if (age == "20代") {
         if (meal == "外食") {
-            message = "今日は外食しました";
+            messageElement.innerHTML = "今日は外食しました";
         } else if (meal == "外食以外") {
-            message = "自炊しました！偉いですね！";
+            messageElement.innerHTML = "自炊しました！偉いですね！";
+            messageElement.className = "special-message";
         }
     } else {
         if (meal == "外食") {
-            message = "今日は外食しました";
+            messageElement.innerHTML = "今日は外食しました";
         } else if (meal == "外食以外") {
-            message = "自炊した可能性はあるかもしれませんね！";
+            messageElement.innerHTML = "自炊した可能性はあるかもしれませんね！";
         }
     }
-
-    document.getElementById("message").innerHTML = message;
 }
